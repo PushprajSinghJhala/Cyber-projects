d54e7828e9d4f2518b9ed7bc0707add367ebdf68
Pushparaj singh Jhala <158456288+PushprajSinghJhala@users.noreply.github.com>
2025-03-09 01:38:21 +0530

    Add files via upload

---
+++ Port scanner.py
@@ -0,0 +1,18 @@
+import socket
+
+def scan_port(target, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print(f"Port {port} is OPEN")
+        s.close()
+    except:
+        pass
+
+target_ip = input("Enter the target IP: ")
+port_range = range(200)
+
+for port in port_range:
+    scan_port(target_ip, port)
